5502a2e186f2fb412a2147ddff4d0f32cf1405d6
Dalana Pasindu <dalana199981@gmail.com>
2021-04-07 14:59:53 +0530

    small fixes

--- src/modules/auth/router.ts
+++ src/modules/auth/router.ts
@@ -1,14 +1,24 @@
 import express from "express";
 import * as authController from './auth.controller'
+import { AuthUser } from "../../db/entity";
 
 const router = express.Router();
 
 
 router.get("/login", (req, res) => res.render("auth/login"));
+router.post("/login", authController.login);
+
+// reset pw
 router.get("/forgot", (req, res) => res.render("auth/forgot"));
 router.post("/forgot", (req, res) => res.render("auth/forgot"));
 
+// preferences
+router.get("/profile", async (req, res) => {
+    res.locals.user = await req.orm.em.findOneOrFail(AuthUser, req.user.uid)
+    res.render("auth/profile")
+});
+router.post("/profile", (req, res) => res.render("auth/profile"));
+
 router.get("/logout", authController.logout);
-router.post("/login", authController.login);
 
 export const AuthModuleRouter = router;
