4691bdd88167ccbcf682bd3d0114adebfbb13982
Dalana Pasindu <dalana199981@gmail.com>
2021-04-10 11:34:34 +0530

    update compression

--- src/middleware/compression.ts
+++ src/middleware/compression.ts
@@ -1,181 +1,180 @@
- 
-var zlib = require("zlib");
-
+import zlib from "zlib";
 // options
-var filter = shouldCompress;
+const filter = shouldCompress;
 let length;
+
 export function CompressionMiddleware(req, res, next) {
-	var ended = false;
-	var listeners = [];
-	var stream;
+    let ended = false;
+    let listeners = [];
+    let stream;
 
-	var _end = res.end;
-	var _on = res.on;
-	var _write = res.write;
+    const _end = res.end;
+    const _on = res.on;
+    const _write = res.write;
 
-	// flush
-	res.flush = function flush() {
-		if (stream) {
-			stream.flush();
-		}
-	};
+    // flush
+    res.flush = function flush() {
+        if (stream) {
+            stream.flush();
+        }
+    };
 
-	// proxy
+    // proxy
 
-	res.write = function write(chunk, encoding) {
-		if (ended) {
-			return false;
-		}
+    res.write = function write(chunk, encoding) {
+        if (ended) {
+            return false;
+        }
 
-		if (!this._header) {
-			this._implicitHeader();
-		}
+        if (!this._header) {
+            this._implicitHeader();
+        }
 
-		return stream ? stream.write(toBuffer(chunk, encoding)) : _write.call(this, chunk, encoding);
-	};
+        return stream ? stream.write(toBuffer(chunk, encoding)) : _write.call(this, chunk, encoding);
+    };
 
-	res.end = function end(chunk, encoding) {
-		if (ended) {
-			return false;
-		}
+    res.end = function end(chunk, encoding) {
+        if (ended) {
+            return false;
+        }
 
-		if (!this._header) {
-			// estimate the length
-			if (!this.getHeader("Content-Length")) {
-				length = chunkLength(chunk, encoding);
-			}
+        if (!this._header) {
+            // estimate the length
+            if (!this.getHeader("Content-Length")) {
+                length = chunkLength(chunk, encoding);
+            }
 
-			this._implicitHeader();
-		}
+            this._implicitHeader();
+        }
 
-		if (!stream) {
-			return _end.call(this, chunk, encoding);
-		}
+        if (!stream) {
+            return _end.call(this, chunk, encoding);
+        }
 
-		// mark ended
-		ended = true;
+        // mark ended
+        ended = true;
 
-		// write Buffer for Node.js 0.8
-		return chunk ? stream.end(toBuffer(chunk, encoding)) : stream.end();
-	};
+        // write Buffer for Node.js 0.8
+        return chunk ? stream.end(toBuffer(chunk, encoding)) : stream.end();
+    };
 
-	res.on = function on(type, listener) {
-		if (!listeners || type !== "drain") {
-			return _on.call(this, type, listener);
-		}
+    res.on = function on(type, listener) {
+        if (!listeners || type !== "drain") {
+            return _on.call(this, type, listener);
+        }
 
-		if (stream) {
-			return stream.on(type, listener);
-		}
+        if (stream) {
+            return stream.on(type, listener);
+        }
 
-		// buffer listeners for future stream
-		listeners.push([type, listener]);
+        // buffer listeners for future stream
+        listeners.push([type, listener]);
 
-		return this;
-	};
+        return this;
+    };
 
-	res.writeHead = createWriteHead(res.writeHead, function onResponseHeaders() {
-		var encoding = res.getHeader("Content-Encoding") || "identity";
-		var method = req.get("Accept-Encoding").includes("gzip") ? "gzip" : "identity";
+    res.writeHead = createWriteHead(res.writeHead, function onResponseHeaders() {
+        const encoding = res.getHeader("Content-Encoding") || "identity";
+        const method = req.get("Accept-Encoding").includes("gzip") ? "gzip" : "identity";
 
-		if (
-			!method ||
-			method === "identity" ||
-			encoding !== "identity" ||
-			req.method === "HEAD" ||
-			!filter(req, res)
-		) {
-			for (var i = 0; i < listeners.length; i++) {
-				_on.apply(stream, listeners[i]);
-			}
-			listeners = null;
-			return;
-		}
+        if (
+            !method ||
+            method === "identity" ||
+            encoding !== "identity" ||
+            req.method === "HEAD" ||
+            !filter(req, res)
+        ) {
+            for (var i = 0; i < listeners.length; i++) {
+                _on.apply(stream, listeners[i]);
+            }
+            listeners = null;
+            return;
+        }
 
-		vary(res, "Accept-Encoding");
+        vary(res, "Accept-Encoding");
 
-		// compression stream
-		stream = method === "gzip" ? zlib.createGzip() : zlib.createDeflate();
+        // compression stream
+        stream = method === "gzip" ? zlib.createGzip() : zlib.createDeflate();
 
-		for (var i = 0; i < listeners.length; i++) {
-			stream.on.apply(stream, listeners[i]);
-		}
+        for (var i = 0; i < listeners.length; i++) {
+            stream.on.apply(stream, listeners[i]);
+        }
 
-		// header fields
-		res.setHeader("Content-Encoding", method);
-		res.removeHeader("Content-Length");
+        // header fields
+        res.setHeader("Content-Encoding", method);
+        res.removeHeader("Content-Length");
 
-		// compression
-		stream.on("data", function onStreamData(chunk) {
-			if (_write.call(res, chunk) === false) {
-				stream.pause();
-			}
-		});
+        // compression
+        stream.on("data", function onStreamData(chunk) {
+            if (_write.call(res, chunk) === false) {
+                stream.pause();
+            }
+        });
 
-		stream.on("end", function onStreamEnd() {
-			_end.call(res);
-		});
+        stream.on("end", function onStreamEnd() {
+            _end.call(res);
+        });
 
-		_on.call(res, "drain", function onResponseDrain() {
-			stream.resume();
-		});
-	});
+        _on.call(res, "drain", function onResponseDrain() {
+            stream.resume();
+        });
+    });
 
-	next();
+    next();
 }
 
 function chunkLength(chunk, encoding) {
-	if (!chunk) {
-		return 0;
-	}
-	return !Buffer.isBuffer(chunk) ? Buffer.byteLength(chunk, encoding) : chunk.length;
+    if (!chunk) {
+        return 0;
+    }
+    return !Buffer.isBuffer(chunk) ? Buffer.byteLength(chunk, encoding) : chunk.length;
 }
 
 function shouldCompress(req, res) {
-	var type = res.getHeader("Content-Type");
+    const type = res.getHeader("Content-Type");
 
-	if (type === undefined || !compressible(type)) {
-		return false;
-	}
+    if (type === undefined || !compressible(type)) {
+        return false;
+    }
 
-	return true;
+    return true;
 }
 
 let toBuffer = (chunk, encoding) =>
-	!Buffer.isBuffer(chunk) ? Buffer.from(chunk, encoding) : chunk;
+    !Buffer.isBuffer(chunk) ? Buffer.from(chunk, encoding) : chunk;
 
 function vary(res, field) {
-	// get existing header
-	var val = res.getHeader("Vary") || "";
-	var header = Array.isArray(val) ? val.join(", ") : String(val);
-	let headers = header.split(", ");
-	headers.push(field);
-	// set new header
-	res.setHeader("Vary", headers.join(", "));
+    // get existing header
+    const val = res.getHeader("Vary") || "";
+    const header = Array.isArray(val) ? val.join(", ") : String(val);
+    let headers = header.split(", ");
+    headers.push(field);
+    // set new header
+    res.setHeader("Vary", headers.join(", "));
 }
 
-var COMPRESSIBLE_TYPE_REGEXP = /^text\/|\+(?:json|text|xml)$/i;
-var EXTRACT_TYPE_REGEXP = /^\s*([^;\s]*)(?:;|\s|$)/;
+const COMPRESSIBLE_TYPE_REGEXP = /^text\/|\+(?:json|text|xml)$/i;
+const EXTRACT_TYPE_REGEXP = /^\s*([^;\s]*)(?:;|\s|$)/;
 
 function compressible(type) {
-	if (!type || typeof type !== "string") {
-		return false;
-	}
-	// strip parameters
-	var match = EXTRACT_TYPE_REGEXP.exec(type);
-	var mime = match && match[1].toLowerCase();
-	// fallback to regexp or unknown
-	return COMPRESSIBLE_TYPE_REGEXP.test(mime) || mime == "application/javascript" || undefined;
+    if (!type || typeof type !== "string") {
+        return false;
+    }
+    // strip parameters
+    const match = EXTRACT_TYPE_REGEXP.exec(type);
+    const mime = match && match[1].toLowerCase();
+    // fallback to regexp or unknown
+    return COMPRESSIBLE_TYPE_REGEXP.test(mime) || mime == "application/javascript" || undefined;
 }
 
 function createWriteHead(prevWriteHead, listener) {
-	var fired = false;
-	return function writeHead(statusCode) {
-		// fire listener
-		if (!fired) {
-			fired = true;
-			listener.call(this);
-		}
-		return prevWriteHead.apply(this, arguments);
-	};
+    let fired = false;
+    return function writeHead(statusCode) {
+        // fire listener
+        if (!fired) {
+            fired = true;
+            listener.call(this);
+        }
+        return prevWriteHead.apply(this, arguments);
+    };
 }
